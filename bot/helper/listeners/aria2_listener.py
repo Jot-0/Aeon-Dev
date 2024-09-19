58044bb221994a152de1f1cc094853238c9b2786
Manjot Singh <97024212+Jot4349@users.noreply.github.com>
2024-09-19 21:05:28 +0530

    Update aria2_listener.py

--- bot/helper/listeners/aria2_listener.py
+++ bot/helper/listeners/aria2_listener.py
@@ -61,10 +61,10 @@ async def _on_download_started(api, gid):
 
     size = download.total_length
     if msg := await check_limits_size(task.listener, size):
-    LOGGER.info("File/folder size over the limit size!")
-    await gather(task.listener.onDownloadError(f"{msg}. File/folder size is {get_readable_file_size(size)}."),
-                 sync_to_async(api.remove, [download], force=True, files=True))
-    return 
+        LOGGER.info("File/folder size over the limit size!")
+        await gather(task.listener.onDownloadError(f"{msg}. File/folder size is {get_readable_file_size(size)}."),
+        await sync_to_async(api.remove, [download], force=True, files=True))
+        return 
 
 @new_thread
 async def _on_download_complete(api, gid):
