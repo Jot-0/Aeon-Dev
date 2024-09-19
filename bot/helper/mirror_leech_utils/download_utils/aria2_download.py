40dc9c23057eaced8a2e4081b9114847469d23e6
Manjot Singh <97024212+Jot4349@users.noreply.github.com>
2024-09-19 15:14:07 +0530

    Update aria2_download.py

--- bot/helper/mirror_leech_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_leech_utils/download_utils/aria2_download.py
@@ -58,8 +58,8 @@ async def add_aria2c_download(listener, dpath, header, ratio, seed_time):
         LOGGER.info(f"Aria2c Download Error: {error}")
         await listener.onDownloadError(error)
         return
-        
-size = download.total_length
+          
+    size = download.total_length
     if msg := await check_limits_size(listener.link, size):
         LOGGER.info("File/folder size over the limit size!")
         await gather(listener.link.onDownloadError(f"{msg}. File/folder size is {get_readable_file_size(size)}."),
