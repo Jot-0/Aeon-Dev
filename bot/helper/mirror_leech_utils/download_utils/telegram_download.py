c94139c25116f55cf056d4a4ec363950606b50f7
Manjot Singh <97024212+Jot4349@users.noreply.github.com>
2024-09-19 12:09:38 +0530

    Update telegram_download.py

--- bot/helper/mirror_leech_utils/download_utils/telegram_download.py
+++ bot/helper/mirror_leech_utils/download_utils/telegram_download.py
@@ -132,7 +132,7 @@ class TelegramDownloadHelper:
                     await self._listener.onDownloadError(msg, button)
                     return
 
-                if msg := await check_limits_size(self._listener, size):
+                if msg := await check_limits_size(self._listener, media.file_size):
                     LOGGER.info("File/folder size over the limit size!")
                     await self._onDownloadError(f"{msg}. File/folder size is {get_readable_file_size(size)}.")
                     return
